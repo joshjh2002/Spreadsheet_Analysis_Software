5f78ed6e78cb50bfd751bf8e38845d1a7a0ad3e4
Joshua Helman <joshhelman@hotmail.co.uk>
2021-01-21 14:56:45 +0000

    Add project files.

---
+++ Spreadsheet_Analysis_Software/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Spreadsheet_Analysis_Software
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spreadsheet_dir_textbox = new System.Windows.Forms.TextBox();
+            this.import_spreadsheet_button = new System.Windows.Forms.Button();
+            this.export_spreadsheet_button = new System.Windows.Forms.Button();
+            this.set_export_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // spreadsheet_dir_textbox
+            // 
+            this.spreadsheet_dir_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spreadsheet_dir_textbox.Location = new System.Drawing.Point(182, 18);
+            this.spreadsheet_dir_textbox.Name = "spreadsheet_dir_textbox";
+            this.spreadsheet_dir_textbox.ReadOnly = true;
+            this.spreadsheet_dir_textbox.Size = new System.Drawing.Size(286, 26);
+            this.spreadsheet_dir_textbox.TabIndex = 0;
+            // 
+            // import_spreadsheet_button
+            // 
+            this.import_spreadsheet_button.Location = new System.Drawing.Point(12, 12);
+            this.import_spreadsheet_button.Name = "import_spreadsheet_button";
+            this.import_spreadsheet_button.Size = new System.Drawing.Size(164, 39);
+            this.import_spreadsheet_button.TabIndex = 1;
+            this.import_spreadsheet_button.Text = "Import Spreadsheet";
+            this.import_spreadsheet_button.UseVisualStyleBackColor = true;
+            this.import_spreadsheet_button.Click += new System.EventHandler(this.import_spreadsheet_button_Click);
+            // 
+            // export_spreadsheet_button
+            // 
+            this.export_spreadsheet_button.Enabled = false;
+            this.export_spreadsheet_button.Location = new System.Drawing.Point(12, 57);
+            this.export_spreadsheet_button.Name = "export_spreadsheet_button";
+            this.export_spreadsheet_button.Size = new System.Drawing.Size(286, 39);
+            this.export_spreadsheet_button.TabIndex = 1;
+            this.export_spreadsheet_button.Text = "Export Spreadsheet";
+            this.export_spreadsheet_button.UseVisualStyleBackColor = true;
+            this.export_spreadsheet_button.Click += new System.EventHandler(this.export_spreadsheet_button_Click);
+            // 
+            // set_export_button
+            // 
+            this.set_export_button.Location = new System.Drawing.Point(304, 57);
+            this.set_export_button.Name = "set_export_button";
+            this.set_export_button.Size = new System.Drawing.Size(164, 39);
+            this.set_export_button.TabIndex = 1;
+            this.set_export_button.Text = "Set Export Location";
+            this.set_export_button.UseVisualStyleBackColor = true;
+            this.set_export_button.Click += new System.EventHandler(this.set_export_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 107);
+            this.Controls.Add(this.set_export_button);
+            this.Controls.Add(this.export_spreadsheet_button);
+            this.Controls.Add(this.import_spreadsheet_button);
+            this.Controls.Add(this.spreadsheet_dir_textbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MSS Software Spreadsheets";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox spreadsheet_dir_textbox;
+        private System.Windows.Forms.Button import_spreadsheet_button;
+        private System.Windows.Forms.Button export_spreadsheet_button;
+        private System.Windows.Forms.Button set_export_button;
+    }
+}
+
---
+++ Spreadsheet_Analysis_Software/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Spreadsheet_Analysis_Software
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int worksheetrows, worksheetcolumns;
+        int rows, columns;
+
+        string[,] resizedWorksheet;
+
+        private void import_spreadsheet_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "Spreadsheet (*.xls)|*.xls| Spreadsheet (*.xlsx)|*.xlsx" };
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                spreadsheet_dir_textbox.Text = ofd.FileName;
+                GetWorksheet(ofd.FileName);
+                MessageBox.Show("Spreadsheet Sucessfully Imported");
+                export_spreadsheet_button.Enabled = true;
+            }
+        }
+
+        private void export_spreadsheet_button_Click(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.default_directory != "no_dir")
+            {
+                string filename = Properties.Settings.Default.default_directory + "/" + DateTime.Now.Month + " " + DateTime.Now.Day + " " + DateTime.Now.Year + "_Report.xlsx";
+
+                //MessageBox.Show(filename);
+
+                Excel.Application xlApp = new Excel.Application();
+
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
+
+                Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+
+                int titleColumn = 0;
+                int sessionColumn = 0;
+                int checkInColumn = 0;
+                for (int i = 0; i <= rows; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        //MessageBox.Show("i, j: " + i + "," + j);
+                        xlWorksheet.Cells[i + 1, j + 1] = resizedWorksheet[i, j];
+
+                        if (resizedWorksheet[i, j] == "Title")
+                        {
+                            titleColumn = j;
+                        }
+                        else if (resizedWorksheet[i, j] == "Session Title")
+                        {
+                            sessionColumn = j;
+                        }
+                        else if (resizedWorksheet[i, j] == "Check In Date")
+                        {
+                            checkInColumn = j;
+                        }
+
+                    }
+                }
+
+                
+
+                //Analyse Data
+                int meals_served = rows;
+                int[] breakfast = new int[3];
+                int[] lunch = new int[3];
+                int[] snack = new int[3];
+
+                for (int i = 0; i <= rows; i++)
+                {
+                    if (resizedWorksheet[i, titleColumn].ToLower() == "paid" && resizedWorksheet[i, sessionColumn].ToLower() == "breakfast session")
+                    {
+                        breakfast[0] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "paid" && resizedWorksheet[i, sessionColumn].ToLower() == "lunch session")
+                    {
+                        lunch[0] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "paid" && resizedWorksheet[i, sessionColumn].ToLower() == "snack session")
+                    {
+                        snack[0] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "reduced" && resizedWorksheet[i, sessionColumn].ToLower() == "breakfast session")
+                    {
+                        breakfast[1] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "reduced" && resizedWorksheet[i, sessionColumn].ToLower() == "lunch session")
+                    {
+                        lunch[1] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "reduced" && resizedWorksheet[i, sessionColumn].ToLower() == "snack session")
+                    {
+                        snack[1] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "free" && resizedWorksheet[i, sessionColumn].ToLower() == "breakfast session")
+                    {
+                        breakfast[2] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "free" && resizedWorksheet[i, sessionColumn].ToLower() == "lunch session")
+                    {
+                        lunch[2] += 1;
+                    }
+                    else if (resizedWorksheet[i, titleColumn].ToLower() == "free" && resizedWorksheet[i, sessionColumn].ToLower() == "snack session")
+                    {
+                        snack[2] += 1;
+                    }
+                }
+
+                int days = -1;
+                for (int i = 0; i <= rows; i++)
+                {
+                    bool found = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (resizedWorksheet[i, checkInColumn] == resizedWorksheet[j, checkInColumn])
+                        {
+                            found = true;
+                        }
+                    }
+                    if (!found)
+                    {
+                        days++;
+                    }
+                }
+
+
+                xlWorksheet.Cells[3, 9] = "Breakfast"; 
+                xlWorksheet.Cells[4, 9] = "Lunch"; 
+                xlWorksheet.Cells[5, 9] = "Snacks"; 
+                xlWorksheet.Cells[6, 9] = "Totals";
+
+                xlWorksheet.Cells[2, 10] = "Paid";
+                xlWorksheet.Cells[2, 11] = "Reduced";
+                xlWorksheet.Cells[2, 12] = "Free";
+                xlWorksheet.Cells[2, 13] = "Totals";
+
+                xlWorksheet.Cells[3, 10] = breakfast[0].ToString();
+                xlWorksheet.Cells[3, 11] = breakfast[1].ToString();
+                xlWorksheet.Cells[3, 12] = breakfast[2].ToString();
+                xlWorksheet.Cells[3, 13] = (breakfast[0] + breakfast[1] + breakfast[2]).ToString();
+
+                xlWorksheet.Cells[4, 10] = lunch[0].ToString();
+                xlWorksheet.Cells[4, 11] = lunch[1].ToString();
+                xlWorksheet.Cells[4, 12] = lunch[2].ToString();
+                xlWorksheet.Cells[4, 13] = (lunch[0] + lunch[1] + lunch[2]).ToString();
+
+                xlWorksheet.Cells[5, 10] = snack[0].ToString();
+                xlWorksheet.Cells[5, 11] = snack[1].ToString();
+                xlWorksheet.Cells[5, 12] = snack[2].ToString();
+                xlWorksheet.Cells[5, 13] = (snack[0] + snack[1] + snack[2]).ToString();
+
+                xlWorksheet.Cells[6, 10] = (breakfast[0] + lunch[0] + snack[0]).ToString();
+                xlWorksheet.Cells[6, 11] = (breakfast[1] + lunch[1] + snack[1]).ToString();
+                xlWorksheet.Cells[6, 12] = (breakfast[2] + lunch[2] + snack[2]).ToString();
+
+                xlWorksheet.Cells[7, 14] = "Total Days:";
+                xlWorksheet.Cells[8, 14] = "Total Meals Served:";
+
+                xlWorksheet.Cells[7, 15] = days;
+                xlWorksheet.Cells[8, 15] = meals_served;
+
+                try
+                {
+                    xlWorkbook.SaveAs(filename);
+                }
+                catch
+                {
+                    MessageBox.Show("The application cannot access this file. Make sure it is not open.");
+                }
+
+                xlApp.Quit();
+
+            }
+            else
+            {
+                MessageBox.Show("You must first set a default directory.");
+            }
+        }
+
+        private void GetWorksheet(string file_dir)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(file_dir);
+
+            Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            
+            rows = xlRange.Rows.Count - 1;
+            columns = xlRange.Columns.Count;
+
+            string[,] worksheet = new string[rows + 2,columns + 2];
+            worksheetrows = rows + 2;
+            worksheetcolumns = worksheetcolumns + 2;
+            //MessageBox.Show("columns: " + columns);
+
+            for (int curCol = 1; curCol <= columns;  curCol++)
+            {
+                for (int curRow = 1; curRow <= rows + 1; curRow++)
+                {
+                    worksheet[curRow - 1, curCol - 1] = xlRange.Cells[curRow, curCol].Value2.ToString() + "";
+                    //MessageBox.Show((curCol - 1 ) + " , " + (curRow - 1) + "   " +  worksheet[curRow - 1, curCol - 1]);
+                }
+            }
+
+            xlWorkbook.Close();
+            xlApp.Quit();
+
+            int numberOfColumnsFound = 0;
+            resizedWorksheet = new string[rows + 1, 7];
+
+            for (int i = 0; i < columns; i++)
+            {
+                if (worksheet[0, i] == "Last Name" || worksheet[0, i] == "First Name" || worksheet[0, i] == "Title" || worksheet[0, i] == "Badge Number" || worksheet[0, i] == "Session ID"
+                    || worksheet[0, i] == "Session Title" || worksheet[0, i] == "Check In Date")
+                {
+                    for (int j = 0; j <= rows; j++)
+                    {
+                        resizedWorksheet[j, numberOfColumnsFound] = worksheet[j, i];
+                        //MessageBox.Show(i + ", " + j + "    " + resizedWorksheet[j, numberOfColumnsFound]);
+                    }
+                    numberOfColumnsFound++;
+                }
+            }
+        }
+
+        private void set_export_button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.default_directory = fbd.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
